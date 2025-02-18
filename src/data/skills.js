267ad4873aa8fadca009570f3957251f883c2e14
Leon Munene <leonmunene@gmail.com>
2025-02-18 08:07:27 +0300

    skiils

--- src/data/skills.js
+++ src/data/skills.js
@@ -13,15 +13,15 @@ import {
   } from "../assets";
   
   const skills = [
-    { name: "JavaScript", icon: javascript },
-    { name: "React", icon: reactjs },
     { name: "Python", icon: python },
+    { name: "React", icon: reactjs },
     { name: "Flask", icon: flask },
+    { name: "Rust", icon: rust },
     { name: "Tailwind CSS", icon: tailwind },
+    { name: "JavaScript", icon: javascript },
     { name: "C++", icon: cplus },
     { name: "Git", icon: git },
     { name: "Docker", icon: docker },
-    { name: "Rust", icon: rust },
     { name: "HTML", icon: html },
     { name: "CSS", icon: css },
   ];
